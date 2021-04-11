f6ee72b1093f91e86421c2a92241fe1fb8a23391
hgrranzi <vkh.mariia@gmail.com>
2021-04-11 20:00:24 +0300

    Inited a and b stacks, check for duplucations needed

--- checker.c
+++ checker.c
@@ -6,25 +6,35 @@
 
 #include "checker.h"
 
-void	display_error()
+void		display_error()
 {
 	write(1, "Error\n", 6);
 	exit(0);
 }
 
-int		main(int argc, char **argv)
+void		checker(t_stack *a, t_stack *b)
 {
-	int	i;
+	return ;
+}
+
+int			main(int argc, char **argv)
+{
+	int		i;
+	t_stack	a;
+	t_stack	b;
 
 	i = 1;
 	if (argc > 1)
 	{
-		write(1, "Checker works\n", 14);
+		if (!(init_stack(&a, argc - 1)) || (!init_stack(&b, argc - 1)))
+			display_error();
 		while (i < argc)
 		{
-			printf("%d\n", aka_atoi(argv[i]));
+			push(&a, aka_atoi(argv[i]));
 			i++;
 		}
+		checker(&a, &b);
+		print_stack(&a);
 	}
 	return (0);
 }
--- checker.h
+++ checker.h
@@ -18,10 +18,20 @@ typedef struct	s_stack
 	int			size;
 }				t_stack;
 
-
 void	display_error();
+void	checker(t_stack *a, t_stack *b);
+
+int		*init_stack(t_stack *stack, int size);
+int		is_empty(t_stack *stack);
+int		is_full(t_stack *stack);
+void	push(t_stack *stack, int element);
+int		pop(t_stack *stack);
+void	destroy_stack(t_stack *stack);
+void	print_stack(t_stack *stack);
 
 int		aka_atoi(char *str);
 int		aka_isdigit(char c);
+void	aka_putchar(char c);
+void	aka_putnbr(int nbr);
 
 #endif
