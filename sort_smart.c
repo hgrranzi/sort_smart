3ada54d1dc669e9de0bd2a06cb8c8dffa535c35d
hgrranzi <vkh.mariia@gmail.com>
2021-04-22 08:43:34 +0300

    Given an index to each element of the initial stack a

--- sort_smart.c
+++ sort_smart.c
@@ -6,7 +6,116 @@
 
 #include "sort_smart.h"
 
-int		main(int argc, char **argv)
+int			*copy_stack_data(t_stack *stack)
+{
+	int		*new_data;
+	int		i;
+
+	new_data = malloc(stack->status);
+	i = 0;
+	if (new_data)
+		while (i < stack->status)
+		{
+			new_data[i] = stack->data[i];
+			i++;
+		}
+	return (new_data);
+}
+
+int			need_swap(int *a, int *b, int flag)
+{
+	int		tmp;
+
+	if (*a > *b)
+	{
+		tmp = *a;
+		*a = *b;
+		*b = tmp;
+		flag = 1;
+	}
+	return (flag);
+}
+
+void		sort_array(int	*array, int size)
+{
+	int		start;
+	int		end;
+	int		flag;
+	int		i;
+
+	start = 0;
+	end  = size - 1;
+	flag = 1;
+	while ((start < end) && flag)
+	{
+		i = start;
+		flag = 0;
+		while (i < end)
+		{
+			flag = need_swap(&array[i], &array[i + 1], flag);
+			i++;
+		}
+		end--;
+		i = end;
+		while (i > start)
+		{
+			flag = need_swap(&array[i - 1], &array[i], flag);
+			i--;
+		}
+		start++;
+	}
+}
+
+void		index_stack(t_stack *stack, int *array)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < stack->status)
+	{
+		j = 0;
+		while (j < stack->status)
+		{
+			if (stack->data[i] == array[j])
+			{
+				stack->data[i] = j;
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
+	free(array);
+	array = NULL;
+}
+
+void		sort_clever(t_info *info)
+{
+	int		*tmp;
+	int		i;
+
+	tmp = copy_stack_data(info->a);
+	if (!tmp)
+		display_error();
+	i = 0;
+	sort_array(tmp, info->a->status); // сортируем стек а
+	index_stack(info->a, tmp); // вместо каждого числа в стеке записываем его номер в отсортированной последовательности
+	// находим наибольшую восходящую последовательность, записываем ее границы
+	// перекидываем в б все кроме последовательности
+		// стоит ли выполнить двойную команду
+		// проверить не расширилась ли последовательность
+	// перекидываем обратно в а
+	// находим наибольшую убывающую последовательность
+		// для каждого числа в b считаем количество шагов до нужной позиции в а
+		// выполняем шаги для того, у кого их меньше
+	//print_stack(info->a);
+	//print_stack(info->a);
+	sort_stupid(info);
+	return ;
+}
+
+int			main(int argc, char **argv)
 {
 	t_info	info;
 	t_stack	a;
@@ -22,12 +131,13 @@ int		main(int argc, char **argv)
 		create_stacks(argv, argc - 1, &a, &b);
 		if (!is_sorted(&a))
 		{
-			cmd.data = init_stack(&cmd, 20 * argc); // that should realloc when necessary
+			cmd.data = init_stack(&cmd, 20 * argc); // that should realloc when necessary or be cleaned
 			info.commands = init_commands(commands, CMD_NUMBER);
 			if (!cmd.data || !info.commands)
 				display_error();
-			sort_stupid(&info);
+			sort_clever(&info);
 		}
+		//print_stack(info.a);
 	}
 	free(a.data); // сократить код
 	free(b.data);
--- sort_smart.h
+++ sort_smart.h
@@ -22,4 +22,9 @@ char	**init_commands(char **commands, int size);
 void	sort_stupid(t_info *data);
 int		find_min(t_stack *stack);
 
+void	sort_clever(t_info *info);
+int		*copy_stack_data(t_stack *stack);
+void	sort_array(int	*array, int size);
+void	index_stack(t_stack *stack, int *array);
+
 #endif
